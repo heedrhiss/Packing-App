a97874ed66e1060f9319b7efdb232634491873c2
Heedrhiss <muyideenidris77@gmail.com>
2024-02-26 13:36:37 +0100

    second push

--- src/App.js
+++ src/App.js
@@ -4,7 +4,43 @@ const initialItems = [
 ];
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Footer />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴Heedrhiss Packing App💼</h1>;
 }
 
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need to pack for your trip😍</h3>
+      <select>
+        {/* <input value={1}>1</input>
+        <input value={2}>2</input>
+        <input value={3}>3</input> */}
+      </select>
+      <input type="text" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Footer() {
+  return (
+    <footer className="stats">
+      <p>You have packed X item(s) out of X items listed.</p>
+    </footer>
+  );
+}
 export default App;
