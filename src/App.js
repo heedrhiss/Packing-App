75ac7ee9234da284fd1d5185e6e62a35d17d5ae1
Heedrhiss <muyideenidris77@gmail.com>
2024-02-27 13:14:33 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState([]);
+  function handleItems(newItem) {
+    setItems((items) => [...items, newItem]);
+  }
+  function handleDelete(id) {
+    setItems((items) => items.filter((item) => id !== item.id));
+  }
+  function handleCheck(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAdd={handleItems} />
+      <PackingList
+        items={items}
+        onDelete={handleDelete}
+        onCheck={handleCheck}
+      />
       <Footer />
     </div>
   );
@@ -20,14 +39,22 @@ function Logo() {
   return <h1>🌴Heedrhiss Packing App💼</h1>;
 }
 
-function Form() {
-  const [quantity, setQuantity] = useState("Quantity");
+function Form({ onAdd }) {
+  const [quantity, setQuantity] = useState(1);
   const [description, setDescription] = useState(null);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+    onAdd(newItem);
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need to pack for your trip😍</h3>
       <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
-        <option value={0}>Quantity</option>
         {Array.from({ length: 25 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -45,18 +72,23 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDelete, onCheck }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDelete={onDelete}
+            onCheck={onCheck}
+          />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDelete, onCheck }) {
   return (
     <li
       key={item.id}
@@ -66,9 +98,15 @@ function Item({ item }) {
           : {}
       }
     >
-      {item.description}
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onCheck(item.id)}
+      />
+      <span>{item.quantity}</span>
+      <span>{item.description}</span>
       <span>
-        <button>X</button>
+        <button onClick={() => onDelete(item.id)}>X</button>
       </span>
     </li>
   );
